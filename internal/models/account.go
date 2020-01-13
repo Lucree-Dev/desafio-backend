cd269cb991e7d7d4dc5e0cf9a86aef20a2427ac1
n0bode <paulodonutduck@gmail.com>
2020-01-13 15:45:20 -0300

    Change fields (passwords, userfield) to omitempty)

--- internal/models/account.go
+++ internal/models/account.go
@@ -4,7 +4,7 @@ type Account struct {
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Birthday  string `json:"birthday"`
-	Password  string `json:"password"`
+	Password  string `json:"password,omitempty"`
 	Username  string `json:"username"`
-	UserID    string `json:"user_id"`
+	UserID    string `json:"user_id,omitempty"`
 }
