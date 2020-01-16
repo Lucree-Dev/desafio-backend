93b71fa510da492d9e14232574601ee3c5428f2f
n0bode <paulodonutduck@gmail.com>
2020-01-16 14:27:14 -0300

    Add rethinkdb and validator packages

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 PIPE_DIR=/tmp/lucree.setup.lock
+arr=("-" "\\" "|" "/" "-" "\\"  "|" "/")
 
 close(){
   rm $PIPE_DIR
-  exit 1
+  exit
 }
 
 error(){
@@ -15,7 +16,30 @@ error(){
   #Show a custom output
   echo -e "$(tput bold)$(tput setaf 1)- ERROR: $1 $(tput sgr0)"
   #Remove FIFO from tmp
-  exit
+  exit 1
+}
+
+spin(){
+  n=0
+  while true; do
+    #Here read waiting (no_blocking) 250ms
+    read -t 0.250 -r line < $PIPE_DIR
+    #If there's a response from FIFO
+    if [ ! -z "$line" ]
+    then
+      echo $line > $PIPE_DIR
+      return
+    fi
+
+    #Below show a simple loading
+    echo -ne "\r$(tput setaf 4) $(tput bold) ${arr[n]} Installing$(tput sgr0)"
+    n=$(((n+1) % ${#arr[@]}))
+
+    if [[ $n -eq ${#arr[@]} ]]
+    then
+      n=0
+    fi
+  done
 }
 
 goget(){
@@ -25,50 +49,20 @@ goget(){
     error "Missing go package"
   fi
 
+  #Save cursor position
+  echo -e "$(tput setaf 3)Installing $2 (https://$1)$(tput sgr0)"
+  tput sc
   #Running process getting in concurrency
   ( 
-    #Send to FIFO response 
-    ( `go get -u $1` && echo yes || echo no ) > $PIPE_DIR
+    #Show Spin
+    spin $1
   ) &
-
-  #Save cursor position
-  tput sc
-  echo -e "$(tput setaf 3)Installing $2 (https://$1)$(tput sgr0)"
-
-  n=0
-  arr=( "\U25F4" "\U25F5" "\U25F6" "\U25F7")
-
-  while true; do
-    if [ ! -e $PIPE_DIR ]
-    then
-      error "Another setup is running"
-      break 
-    fi
-
-    #Here read waiting (no_blocking) 100ms
-    read -t 0.1 -r line < $PIPE_DIR
-    #If there's a response from FIFO
-    if [ ! -z "$line" ]
-    then
-      case $line in
-      "no") error "Install $1";;  #Response is Error
-      "yes") break;;               #Response is OK
-      esac
-    fi
-
-    #Below show a simple loading
-    echo -ne "\r$(tput setaf 4) $(tput bold) ${arr[n]} Installing$(tput sgr0)"
-    n=$(((n+1) % 4))
-
-    if [[ $n -eq 5 ]]
-    then
-      n=0
-    fi
-  done
+  ( `go get -u $1` && echo yes || echo no ) > $PIPE_DIR
   
+  < $PIPE_DIR
+
   tput rc
   tput ed
-
   #! `go get -u $1` && error "Install $1"
   echo -e "$(tput setaf 2)+ $2 Installed"
 }
@@ -88,10 +82,12 @@ setup() {
 
   echo "Installing Go packages"
   goget "github.com/go-chi/chi" "GoChi"
-  goget "github.com/go-chi/auth/jwt" "GoChi-JWT"
+  goget "github.com/go-chi/jwtauth" "GoChi-JWT"
   goget "github.com/dgrijalva/jwt-go" "GO-JWT"
   goget "github.com/go-chi/render" "GoChi-Render"
-  goget "go.mongodb.org/mongo-driver/mongo" "GoMongoDrive"
+  goget "gopkg.in/go-playground/validator.v9" "Validator"
+  goget "gopkg.in/rethinkdb/rethinkdb-go.v6" "RethinkdbDriver"
+  goget "github.com/spf13/viper" "Viper"
   echo "$(tput bold)$(tput setaf 2)ALL PACKAGES INSTALLED!$(tput sgr0)"
   close
 }
