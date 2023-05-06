15181b7ff72c5ebcddf77e1f9ffd88ab81304eb9
Marcos Pinho <marcosjava2008@gmail.com>
2023-05-05 21:07:17 -0300

    refactor: Making code improvement

--- internal/application/server/router.go
+++ internal/application/server/router.go
@@ -6,8 +6,12 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
-func RegisterEndPoints(e *echo.Echo) {
+func RegisterEndPoints() *echo.Echo {
+	e := echo.New()
+
 	e.GET("", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
+
+	return e
 }
--- internal/application/server/server.go
+++ internal/application/server/server.go
@@ -1,13 +1,5 @@
 package server
 
-import (
-	"github.com/labstack/echo/v4"
-)
-
 func Start() {
-	e := echo.New()
-
-	RegisterEndPoints(e)
-
-	e.Start(":8080") //TODO pegar a porta definida no arquivo application
+	RegisterEndPoints().Start(":8080") //TODO pegar a porta definida no arquivo application
 }
